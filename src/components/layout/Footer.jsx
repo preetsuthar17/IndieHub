4442940ba3961d580ad54bd4099dd775c8fd821f
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-25 22:03:33 +0530

    Add Header and Footer components, update Layout to include them, and clean up global styles

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <p>
+          Built with ❤️ by <Link href="https://x.com/nott_preett">preett</Link>
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
