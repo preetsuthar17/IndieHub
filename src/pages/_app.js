1c55f3e2d35472a9b299940b63fde037c0fe95c8
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-25 21:48:41 +0530

    Refactor layout structure, remove unused API route, and add theme provider and mode toggle components

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
 import "@/styles/globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider";
+import Layout from "@/components/layout/Layout";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
