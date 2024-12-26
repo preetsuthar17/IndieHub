86fe70192246e1db4c4c49c4bc0a2e61ba15c183
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-26 12:50:33 +0530

    Add BackgroundParticles and MeteorShower components to enhance visual effects in Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,9 @@ import { LucideGithub } from "lucide-react";
 
 import Link from "next/link";
 
+import BackgroundParticles from "./design/BackgroundParticles";
+import MeteorShower from "./design/MeteorShower";
+
 const Hero = () => {
   const [stars, setStars] = useState(0);
 
@@ -17,8 +20,14 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className="relative py-[10rem] flex items-center justify-center overflow-hidden w-[90%] my-[3rem] mx-auto rounded-3xl ">
-      <div class="absolute inset-0 bg-gradient-to-bl from-[#4f0000] via-[#934b1a] to-[#A4508B] opacity-40 blur-[120px] grainy-texture"></div>
+    <section className="relative py-[10rem] my-[3rem] flex items-center justify-center overflow-hidden h-auto w-[90%] mx-auto rounded-3xl min-h-[calc(100vh-10rem)] ">
+      <div className="absolute w-[90rem] rotate-180 bottom-[-30rem] blur-[80px]  rounded-full h-[40rem] bg-primary opacity-10"></div>
+      <div class="absolute inset-0 opacity-70">
+        <BackgroundParticles quantity={300} />
+      </div>
+      <div className="opacity-40">
+        <MeteorShower number={5} />
+      </div>
       <div className="container px-4 md:px-6 relative z-10">
         <div className="flex flex-col items-center space-y-4 text-center">
           <h1 className="text-4xl font-extrabold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-foreground font-sans max-w-[48rem] lg:leading-tight">
@@ -35,8 +44,7 @@ const Hero = () => {
               href="/#resources"
               className="flex items-center gap-1 grow text-sm font-sans underline underline-offset-4 font-medium text-center justify-center"
             >
-              <LucideGithub size={17} />
-              {stars} Star on GitHub
+             Star on GitHub
             </Link>
             <Link
               href="/#resources"
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ export default function Home() {
     <>
       <section>
         <Hero />
-        <hr className="opacity-50" />
         <Resources />
       </section>
     </>
