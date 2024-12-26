88ea85bf99bda442e635f972bef306797c1fcca3
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-26 10:19:08 +0530

    Update default theme in ThemeProvider to 'dark' for improved user experience

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,7 @@ export default function App({ Component, pageProps }) {
     <>
       <ThemeProvider
         attribute="class"
-        defaultTheme="system"
+        defaultTheme="dark"
         enableSystem
         disableTransitionOnChange
       >
