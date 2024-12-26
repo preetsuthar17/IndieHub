fb90dc0fd5fc5f01d39978d1b2e5aad35a3eb55d
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-26 11:08:13 +0530

    Add next-seo package and configure SEO metadata for improved page visibility

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,8 @@ import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Layout from "@/components/layout/Layout";
 
+import { NextSeo } from "next-seo";
+
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -12,6 +14,10 @@ export default function App({ Component, pageProps }) {
         enableSystem
         disableTransitionOnChange
       >
+        <NextSeo
+          title="IndieHub - The only Hub made for Indie Hackers"
+          description="All the resources you need to build and grow your next project."
+        />
         <Layout>
           <Component {...pageProps} />
         </Layout>
