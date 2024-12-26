40f9ed1348461abac7a5a93ea5188a03084e8c11
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-26 11:25:56 +0530

    Add AI and UI components with descriptions, links, and images for enhanced resource management

--- src/data/ai.js
+++ src/data/ai.js
@@ -1,10 +1,52 @@
 const ai = [
-    {
-        name: "Cursor AI",
-        description: "The AI Code Editor Built to make you extraordinarily productive, Cursor is the best way to code with AI.",
-        link: "cursor.com",
-        image: "https://i.imgur.com/EnKRp9U.jpeg",
-    }
-]
+  {
+    name: "Cursor AI",
+    description:
+      "The AI Code Editor Built to make you extraordinarily productive, Cursor is the best way to code with AI.",
+    link: "cursor.com",
+    image: "https://i.imgur.com/EnKRp9U.jpeg",
+  },
+  {
+    name: "Bolt AI",
+    description: "Prompt, run, edit & deploy web apps",
+    link: "bolt.new",
+    image: "https://i.imgur.com/GrMYTF3.png",
+  },
+  {
+    name: "v0",
+    description:
+      "Chat with v0. Generate UI with simple text prompts. Copy, paste, ship.",
+    link: "v0.dev",
+    image: "https://i.imgur.com/hD7BxAD.png",
+  },
+  {
+    name: "Windsurf AI",
+    description:
+      "The first agentic IDE, and then some. The Windsurf Editor is where the work of developers and AI truly flow together, allowing for a coding experience that feels like literal magic.",
+    link: "codeium.com/windsurf",
+    image: "https://i.imgur.com/IRygz8i.png",
+  },
+  {
+    name: "Replit Agent",
+    description:
+      "Create beautiful, modern web applications at the speed of thought. Describe what you need and Replit's AI Agent builds it for you.",
+    link: "replit.com",
+    image: "https://i.imgur.com/IRq5Xcs.png",
+  },
+  {
+    name: "Google IDX",
+    description:
+      "Project IDX is an AI-assisted workspace for full-stack, multiplatform app development in the cloud.",
+    link: "idx.dev",
+    image: "https://i.imgur.com/h7ZZAk8.png",
+  },
+  {
+    name: "GitHub Copilot",
+    description:
+      "GitHub Copilot is an AI pair programmer that helps you write code faster.",
+    link: "copilot.github.com",
+    image: "https://i.imgur.com/XT3ZdkI.png",
+  },
+];
 
 export default ai;
--- src/data/ui.js
+++ src/data/ui.js
@@ -24,8 +24,49 @@ const ui = [
     name: "Magic UI",
     description:
       "50+ free and open-source animated components built with React, Typescript, Tailwind CSS, and Framer Motion.Perfect companion for shadcn/ui.",
-      link: "magicui.design",
-      image: "https://i.imgur.com/Wg6IAJa.png"
+    link: "magicui.design",
+    image: "https://i.imgur.com/Wg6IAJa.png",
+  },
+  {
+    name: "DaisyUI",
+    description:
+      "daisyUI adds component class names to Tailwind CSS so you can make beautiful websites faster than ever.",
+    link: "daisyui.com",
+    image: "https://i.imgur.com/071zDkq.png",
+  },
+  {
+    name: "FloatUI",
+    description:
+      "Beautiful and responsive UI components and templates for React and Vue (soon) with Tailwind CSS.",
+    link: "floatui.com",
+    image: "https://i.imgur.com/1Gq0BS7.png",
+  },
+  {
+    name: "FlowBite",
+    description:
+      "Open-source UI component library and front-end development framework based on Tailwind CSS",
+    link: "flowbite.com",
+    image: "https://i.imgur.com/eOM9v3Z.png",
+  },
+  {
+    name: "ChakraUI",
+    description:
+      "Simple, Modular & Accessible UI Components for your React Applications",
+    link: "chakra-ui.com",
+    image: "https://i.imgur.com/CofsBTc.png",
+  },
+  {
+    name: "Ant Design",
+    description: "An enterprise-class UI design language and React UI library.",
+    link: "ant.design",
+    image: "https://i.imgur.com/6ZnReo2.png",
+  },
+  {
+    name: "MUI",
+    description:
+      "MUI is the organization behind popular open-source projects like Material UI, Base UI, MUI X.",
+    link: "mui.com",
+    image: "https://i.imgur.com/6BmA5yc.png",
   },
 ];
 
