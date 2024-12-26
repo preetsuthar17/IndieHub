a5ca8919d36624c23a41a48e9add7b7022fa84da
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-26 11:05:18 +0530

    Add AI and UI data modules with descriptions, links, and images for enhanced resource management

---
+++ src/data/ai.js
@@ -0,0 +1,10 @@
+const ai = [
+    {
+        name: "Cursor AI",
+        description: "The AI Code Editor Built to make you extraordinarily productive, Cursor is the best way to code with AI.",
+        link: "cursor.com",
+        image: "https://i.imgur.com/EnKRp9U.jpeg",
+    }
+]
+
+export default ai;
---
+++ src/data/ui.js
@@ -0,0 +1,32 @@
+const ui = [
+  {
+    name: "hextastudio/ui",
+    description:
+      "Build Stunning Websites Without The Hassle. Modern, responsive, and customizable UI components for Next.js. Copy, paste, and make it your own.",
+    link: "ui.hextastudio.in",
+    image: "https://i.imgur.com/01l51q2.jpeg",
+  },
+  {
+    name: "shadcn/ui",
+    description:
+      "Beautifully designed components that you can copy and paste into your apps.",
+    link: "ui.shadcn.com",
+    image: "https://i.imgur.com/fYe5mvJ.png",
+  },
+  {
+    name: "Aceternity UI",
+    description:
+      "Copy paste the most trending components and use them in your websites without having to worry about styling and animations.",
+    link: "ui.aceternity.com",
+    image: "https://i.imgur.com/ASSHAVx.png",
+  },
+  {
+    name: "Magic UI",
+    description:
+      "50+ free and open-source animated components built with React, Typescript, Tailwind CSS, and Framer Motion.Perfect companion for shadcn/ui.",
+      link: "magicui.design",
+      image: "https://i.imgur.com/Wg6IAJa.png"
+  },
+];
+
+export default ui;
