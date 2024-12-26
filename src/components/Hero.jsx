4b948f67eb84d7e14a3df29b84ec8cdc3988376b
preetsuthar17 <preetsutharxd@gmail.com>
2024-12-26 12:10:13 +0530

    Remove framer-motion animations from Hero component for simplified rendering

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { motion } from "framer-motion";
 
 import { LucideGithub } from "lucide-react";
 
@@ -20,12 +19,7 @@ const Hero = () => {
   return (
     <section className="relative py-[10rem] flex items-center justify-center overflow-hidden w-[90%] my-[3rem] mx-auto rounded-3xl ">
       <div class="absolute inset-0 bg-gradient-to-bl from-[#4f0000] via-[#934b1a] to-[#A4508B] opacity-40 blur-[120px] grainy-texture"></div>
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-        className="container px-4 md:px-6 relative z-10"
-      >
+      <div className="container px-4 md:px-6 relative z-10">
         <div className="flex flex-col items-center space-y-4 text-center">
           <h1 className="text-4xl font-extrabold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl text-foreground font-sans max-w-[48rem] lg:leading-tight">
             The only{" "}
@@ -52,7 +46,7 @@ const Hero = () => {
             </Link>
           </div>
         </div>
-      </motion.div>
+      </div>
     </section>
   );
 };
